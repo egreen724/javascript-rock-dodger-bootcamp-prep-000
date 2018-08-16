f371deb2ef54078cd50f77cfb9c425a1ceffe252
Erin Green <erin.y.green@gmail.com>
2018-08-16 15:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,16 +45,9 @@ function createRock(x) {
   // Hmmm, why would we have used `var` here?
   var top = 0
 
-  rock.style.top = top
+  rock.style.top = top}
+  
 $("GAME").append(rock); 
-
-
-
-  /**
-   * Now that we have a rock, we'll need to append
-   * it to GAME and move it downwards.
-   */
-
 
   /**
    * This function moves the rock. (2 pixels at a time
